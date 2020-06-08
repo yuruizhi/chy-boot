777ce8d449cc1e835c6a1a953943338d1c1a28f5
Henry.Yu <282373647@qq.com>
2020-06-08 11:57:56 +0800

    UPDATE SOME CODE

--- chy-boot-common/src/main/java/com/changyi/chy/commons/platform/auth/entity/AuthParam.java
+++ chy-boot-common/src/main/java/com/changyi/chy/commons/platform/auth/entity/AuthParam.java
@@ -6,9 +6,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 /**
