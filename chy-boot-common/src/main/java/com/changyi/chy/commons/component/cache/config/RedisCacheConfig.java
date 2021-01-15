1c5a0141e718f2574a180fffd2d8a6666617d0f2
ZhangHao <376981735@qq.com>
2021-01-15 19:19:25 +0800

    perf: 移除不必要的代码，替换过时的方法

--- chy-boot-common/src/main/java/com/changyi/chy/commons/component/cache/config/RedisCacheConfig.java
+++ chy-boot-common/src/main/java/com/changyi/chy/commons/component/cache/config/RedisCacheConfig.java
@@ -3,6 +3,7 @@ package com.changyi.chy.commons.component.cache.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -32,7 +33,7 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
 
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
 
         jackson2JsonRedisSerializer.setObjectMapper(om);
         template.setValueSerializer(jackson2JsonRedisSerializer);
@@ -41,25 +42,13 @@ public class RedisCacheConfig extends CachingConfigurerSupport {
         return template;
     }
 
-   /* @Bean
-    public CacheManager cacheManager(RedisConnectionFactory factory) {
-        RedisCacheManager cacheManager = RedisCacheManager.create(factory);
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
-
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer)).entryTtl(Duration.ofDays(2));
-        cacheManager.getCacheConfigurations()
-        return cacheManager;
-    }*/
-
-
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
 
         RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair
