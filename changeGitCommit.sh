e8750ad35ad155be92d61bb4a15eab3aefdb68b7
yuruizhi <282373647@qq.com>
2025-03-17 16:13:13 +0800

    包统一规划

---
+++ changeGitCommit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+# 之前不规范的邮箱
+OLD_EMAIL="yuruizhi@vchangyi.com"
+
+# 修改后的正确邮箱
+CORRECT_EMAIL="282373647@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
